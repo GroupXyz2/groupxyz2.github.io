a622fbffb9045c1a6d3cc783da8ef4ba5afb29b9
GroupXyz <122353547+GroupXyz2@users.noreply.github.com>
2025-02-14 12:55:19 +0000

    Update scripts.js
    removed captcha

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,3 @@
-window.onload = function() {
-    let captchaPassed = document.cookie.split(';').some((item) => item.trim().startsWith('captcha_passed='));
-    
-    if (!captchaPassed) {
-        window.location.href = "/captcha/vietcong_captcha";
-    }
-}
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('.animated-button');
 
